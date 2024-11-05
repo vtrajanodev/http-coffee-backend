8141af255b6cc517288d865eb4115df621354b94
victor.trajano <victor.dasilva@consulting-for.edenred.com>
2024-11-04 23:12:15 -0300

    feat teste

--- server.js
+++ server.js
@@ -18,16 +18,3 @@ app.get('/api/coffees', (req, res) => {
 app.listen(5000, () => {
   console.log('Servidor rodando na porta 5000');
 });
-
-app.get('/api/coffees', (req, res) => {
-  const coffees = [
-    { id: 1, name: 'Expresso Tradicional', price: 4.00 },
-    { id: 2, name: 'Expresso Americano', price: 7.90 },
-    { id: 3, name: 'Expresso Cremoso', price: 9.90 },
-  ];
-  res.status(200).json({ coffees });
-});
-
-app.listen(5000, () => {
-  console.log('Servidor rodando na porta 5000');
-});
